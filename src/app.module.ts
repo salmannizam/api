9e9a5e34aa1b5fe3dd3ea3e7a1ecfb2ff3e37656
salman nizam <salmannizam.info@gmail.com>
2025-01-02 22:10:22 +0530

    commnetd db chnges

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,20 +14,20 @@ import { AuthModule } from './auth/auth.module';
     }),
     // AuthModule,
     UserModule,
-    TypeOrmModule.forRoot({
-      type: 'mssql',
-      host: process.env.DB_HOST, // Use the environment variables
-      port: parseInt(process.env.DB_PORT), // Convert to integer
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      extra: {
-        encrypt: false, // Disable encryption for local testing
-        trustServerCertificate: true, // Trust self-signed certificates
-      },
-      entities: [__dirname + '/**/*.entity{.ts,.js}'], // Automatically load entities
-      synchronize: process.env.DB_SYNCHRONIZE === 'true', // Use environment variable for synchronization
-    }),
+    // TypeOrmModule.forRoot({
+    //   type: 'mssql',
+    //   host: process.env.DB_HOST, // Use the environment variables
+    //   port: parseInt(process.env.DB_PORT), // Convert to integer
+    //   username: process.env.DB_USERNAME,
+    //   password: process.env.DB_PASSWORD,
+    //   database: process.env.DB_DATABASE,
+    //   extra: {
+    //     encrypt: false, // Disable encryption for local testing
+    //     trustServerCertificate: true, // Trust self-signed certificates
+    //   },
+    //   entities: [__dirname + '/**/*.entity{.ts,.js}'], // Automatically load entities
+    //   synchronize: process.env.DB_SYNCHRONIZE === 'true', // Use environment variable for synchronization
+    // }),
     AuthModule,
   ],
 })
