705ff8001b5b61be77a91a47ec616592e3593649
salmannizam <salmannizam.info@gmail.com>
2025-01-25 10:11:56 +0530

    app controller added in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,27 +6,18 @@ import { DatabaseModule } from './database/database.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { ConfigModule } from '@nestjs/config'; // Import the ConfigModule
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 @Module({
-  imports: [SurveyModule, 
+  controllers: [AppController],
+  imports: [SurveyModule,
     DatabaseModule,
-    // ConfigModule.forRoot({
-    //   isGlobal: true, // Make the configuration globally available
-    // }),
-    //     TypeOrmModule.forRoot({
-    //   type: 'mssql',
-    //   host: '40.127.190.1' ,// process.env.DB_HOST, // Use the environment variables
-    //   // port: parseInt(process.env.DB_PORT), // Convert to integer
-    //   username: 'survey', //process.env.DB_USERNAME,
-    //   password: 'survey@123', // process.env.DB_PASSWORD,
-    //   database: 'survey', // process.env.DB_DATABASE,
-    //   extra: {
-    //     encrypt: false, // Disable encryption for local testing
-    //     trustServerCertificate: true, // Trust self-signed certificates
-    //   },
-    //   entities: [__dirname + '/**/*.entity{.ts,.js}'], // Automatically load entities
-    //   synchronize: process.env.DB_SYNCHRONIZE === 'true', // Use environment variable for synchronization
-    // }),
-   ],
+    ConfigModule.forRoot({
+      isGlobal: true, // Make the ConfigModule global
+    }),
+
+  ],
+  providers: [AppService],
 })
 export class AppModule { }
 
